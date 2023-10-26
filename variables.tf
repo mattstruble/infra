7c4712a6b65318c458da9670b01ea53969c5c647
Matt Struble <mattstruble@outlook.com>
2023-10-25 21:48:04 -0400

    fix(cloudflare): remove unnecessary account_id variable
    Signed-off-by: Matt Struble <mattstruble@outlook.com>

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "cloudflare_account_id" {
-  type        = string
-  description = "Account ID to manage the zone resource in."
-  sensitive   = true
-}
-
 variable "cloudflare_api_token" {
   type        = string
   description = "API token to access cloudflare with"
